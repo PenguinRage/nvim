42ea27037f336311a3ed44b1f1dcbf3e538f6ae7
PenguinRage <3924194+PenguinRage@users.noreply.github.com>
2025-02-01 21:24:42 +1100

    asdfasdfasdfasdfasdfasdfadsf

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NVIM Files
+
+Testing this markdown linter.
+
+## Linters required
+
+- markdownlint (Markdown)
+- hadolint (Dockerfile)
+- flake8 (Python)
+- tflint (Terraform)
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        dockerfile = { 'hadolint' },
+        terraform = { 'tflint' },
+        python = { 'flake8' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
